c14ff02b6740f26d3195d35b956ed0c13f1be230
Chase McFaddin <comcfadd@gmail.com>
2020-07-23 21:24:55 -0700

    js complete

---
+++ js/app.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var answerOne = confirm('Are you ready to rumble??');
+console.log(answerOne);
+
+if (answerOne === true) {
+  console.log('Let\'s get ready to rumble!');
+  else {
+    console.log('Ok, loser. :(');
+  }
+
+  var myBool = true;
+  var anotherBool = false;
+  var thirdBool = true;
+
+  if (myBool && thirdBool)
+  console.log ('myBool and thirdBool are true');
+}
+
+else if (myBool || anotherBool) {
+  console.log ('myBool OR anotherBool is true');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
